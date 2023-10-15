c98fccd46c47313183fca435b0a53ea8166290c5
Rohan Doshi <rohan.doshi02@gmail.com>
2023-10-15 14:11:51 +0530

    Support for os.Stdin

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -11,38 +12,52 @@ func main() {
 	printLength := false
 	printWordCount := false
 	printLineCount := false
+	printFile := true
 
-	if len(os.Args) == 1 {
-		log.Fatal("No file specified")
+	// if len(os.Args) == 1 {
+	// 	log.Fatal("No file specified")
+	// }
+	var stringContent string
+	var count int
+	noStdin := false
+
+	filePath := os.Args[len(os.Args)-1]
+	if filePath == "-l" || filePath == "-c" || filePath == "-w" || filePath == os.Args[0] {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			stringContent += scanner.Text() + "\n"
+		}
+		printFile = false
+		count = len(os.Args)
+	} else {
+		// Read File
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println("The file name should be the last argument")
+			log.Fatal(err)
+		}
+		stringContent = string(content)
+		count = len(os.Args) - 1
+		noStdin = true
 	}
 
-	if len(os.Args) != 2 {
-		for i := 1; i < len(os.Args)-1; i++ {
+	if len(os.Args) == 1 || (noStdin && len(os.Args) == 2) {
+		printLength = true
+		printWordCount = true
+		printLineCount = true
+	} else {
+		for i := 1; i < count; i++ {
 			if os.Args[i] == "-l" {
-				printWordCount = true
+				printLineCount = true
 			} else if os.Args[i] == "-c" {
 				printLength = true
 			} else if os.Args[i] == "-w" {
 				printWordCount = true
+			} else {
+				log.Fatal("Invalid flag, only -l -c -w are supported")
 			}
 		}
-	} else {
-		printLength = true
-		printWordCount = true
-		printLineCount = true
 	}
-
-	filePath := os.Args[len(os.Args)-1]
-
-	// Read File
-	content, err := os.ReadFile(filePath)
-	if err != nil {
-		fmt.Println("The file name should be the last argument")
-		log.Fatal(err)
-	}
-
-	// Convert to string
-	stringContent := string(content)
 
 	// Get all lines
 	stringSlice := strings.Split(stringContent, "\n")
@@ -68,11 +83,14 @@ func main() {
 
 	if printLength {
 		outputText += "\t" + "Bytes"
-		output += "\t" + fmt.Sprint(len(content))
+		output += "\t" + fmt.Sprint(len(stringContent))
 	}
 
-	outputText += "\t" + "File"
-	output += "\t" + filePath
+	if printFile {
+		outputText += "\t" + "File"
+		output += "\t" + filePath
+	}
+
 	fmt.Println(outputText)
 	fmt.Println(output)
 }
